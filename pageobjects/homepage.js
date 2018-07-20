6396f4985a4befabc9682d79f016d45b4067266b
Ahmed Moeed Hashmi <ahmed.moeed@zameen.com>
2018-07-20 12:28:05 +0500

    Home Page - Tests

---
+++ pageobjects/homepage.js
@@ -0,0 +1,82 @@
+var page = require('./page');
+var assert = require('assert');
+//var prop = require('./prop');
+
+//Page objects
+var saleButton = '//*[@class="b7afbb84"]'
+var locationBar = '//*[@placeholder="Location"]'
+var locationIndex = '//li[@data-selected="true"]'
+var moreOption = '//span[@aria-label="Collapse expand button"]'
+var moreFilteroptions = '//span[@class="_514754a1"]'
+var propertyTypebox = '//div[@name="property type"]'
+var propertyTypes = '//div[@class="_97ab0b50"]'
+var priceFilter = '//div[@aria-label="Price filter"]'
+var minPricerange = '//button[@aria-label="200,000"]'
+var maxPricerange = '//div[@role="listbox"]//div[@class="ef391b9a"][2]'
+var bedsFilter = '//div[@aria-label="Bed filter"]'
+var selectedBeds = '//*[@aria-label="5"]'
+var findButton = '//a[@role="button"]'
+var closeBedsfilter = '//button[@class="_85b30621"]'
+
+//Page Functions
+var homepage = Object.create(page, { 
+
+    salebtn : {get: function() {return $(saleButton)}},
+
+    enterlocation : {get: function() {homepageassertions.entertext1}},
+    
+    clickmoreoption :{get: function() {
+        var expectedoption = 'More Options'
+        if(browser.getText(moreFilteroptions) == expectedoption)
+        {
+            return $(moreOption).click()
+        }
+        else{
+            console.log("More Filters are already viewable")
+        }
+    }},
+
+    selectpropertytype : {get: function() {
+        $(propertyTypebox).click();
+        //Saving the entire residential list
+        var getPropertytype = $(propertyTypes);
+        //Select the property type available on the index 1
+        getPropertytype.$$('li')[1].click();
+    }},
+
+    selectprice : {get: function(){
+        $(priceFilter).click();
+        $(minPricerange).click();
+        console.log($(minPricerange).getText() + ' is selected as minimum price')
+        //Saving the entire max price range
+        var getMaxpricelist = $(maxPricerange);
+        //Select the max price from the list
+        getMaxpricelist.$$('button')[16].click();
+        var selectMaxprice = getMaxpricelist.$$('button')[16];
+        console.log(selectMaxprice.getText() + ' is selected as the max price');
+    }},
+
+    selectbeds : {get: function(){
+        $(bedsFilter).click();
+        $(selectedBeds).click();
+        $(closeBedsfilter).click();
+        //console.log(selectedbeds.getText() + ' is selected as the no. of beds')
+    }},
+
+    clickfindbutton : {get: function(){
+        $(findButton).click();
+        console.log(browser.getTitle());
+    }}
+})
+
+var homepageassertions = Object.create(page, {
+    entertext1: {get: function(text1){
+        var text = $(locationBar).setValue("Dubai")
+        browser.pause(2000);
+        $(locationIndex).click();
+    }}
+
+
+})
+
+module.exports = homepage;
---
+++ specs/searchpage.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var homepage = require('../pageobjects/homepage');
+
+describe('homepage', function() {
+    it('should open the website and get title', function() {
+        browser.url('https://sl:getin1@bayut-development.herokuapp.com/');
+        console.log(browser.getTitle());
+    })
+        
+    it ('should click on for sale', function(){
+       homepage.salebtn.click();
+       console.log ('For Sale button is clicked')
+    })
+
+    it ('Enter dubai in location bar', function(){
+        homepage.enterlocation;
+        console.log('Dubai entered in the location box')
+    })
+
+    it ('Select values from more filters', function(){
+        browser.pause(2000);
+        homepage.clickmoreoption;
+        console.log('More option button is clicked to view other filters');
+    })
+
+    it ('Select Property Type', function(){
+        browser.pause(2000);
+        homepage.selectpropertytype;
+    })
+
+    it('Select min and max price', function(){
+        homepage.selectprice;
+    })
+
+    it('Select beds', function(){
+        homepage.selectbeds;
+    })
+
+    it('Click on find button', function(){
+        homepage.clickfindbutton;
+    })
+});
